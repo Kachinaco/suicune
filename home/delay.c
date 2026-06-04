d65860c40f4b4c9ff6c420f3d5e7f41a56c6810a
Cory Analla <coryanalla@gmail.com>
2026-06-04 14:04:32 -0700

    Restore original GBC browser speed

--- home/delay.c
+++ home/delay.c
@@ -3,6 +3,10 @@
 #include "vblank.h"
 #if defined(__EMSCRIPTEN__)
 #include <emscripten.h>
+
+#define GBC_FRAME_MS ((1000.0 * 70224.0) / 4194304.0)
+
+static double sNextBrowserFrameMs;
 #endif
 
 void DelayFrame(void){
@@ -12,7 +16,18 @@ void DelayFrame(void){
     gb.display.window_clear = 0;
     gb_finish_frame();
 #if defined(__EMSCRIPTEN__)
-    emscripten_sleep(16);
+    double now = emscripten_get_now();
+
+    if (sNextBrowserFrameMs <= 0.0 || now > sNextBrowserFrameMs + (GBC_FRAME_MS * 4.0)) {
+        sNextBrowserFrameMs = now + GBC_FRAME_MS;
+    }
+
+    int sleepMs = (int)(sNextBrowserFrameMs - now + 0.5);
+    if (sleepMs > 0) {
+        emscripten_sleep(sleepMs);
+    }
+
+    sNextBrowserFrameMs += GBC_FRAME_MS;
 #endif
 }
 
